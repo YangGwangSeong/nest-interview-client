f0aa27cda4b269cecf9751ffdab15229da2bd954
YangGwangSeong <52439610+YangGwangSeong@users.noreply.github.com>
2022-06-30 19:18:08 +0900

    Revert "feat : install tailwind.css"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <div className="text-3xl font-bold underline">메인페이지</div>
+      <div>메인페이지</div>
     </div>
   );
 }
