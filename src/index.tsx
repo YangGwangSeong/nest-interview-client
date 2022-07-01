1d3587ccd0194b84288c99df86fca4ff75e0e729
YangGwangSeong <52439610+YangGwangSeong@users.noreply.github.com>
2022-07-01 16:28:17 +0900

    Revert "Revert "feat : install tailwind.css""

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-const test = "what talk about";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
